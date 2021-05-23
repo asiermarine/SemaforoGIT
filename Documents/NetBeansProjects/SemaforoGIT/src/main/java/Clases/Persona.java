6baaab5e94fee786602ec7d02199cdc748da2847
Vespertino <Vespertino@DESKTOP-5LQNC3U.localdomain>
2021-05-23 23:42:22 +0200

    JavaDoc añadido a clase PERSONA

---
+++ Documents/NetBeansProjects/SemaforoGIT/src/main/java/Clases/Persona.java
@@ -0,0 +1,10 @@
+package clases;
+/**
+ * Esta será la clase padre de ESTUDAINTE y PROFESOR
+ * @author Asier Mariné 1ºDAW
+ * @version 15.0.1
+ */
+public class Persona {
+	int i_Edad;	
+	String s_Nombre;		
+    }
