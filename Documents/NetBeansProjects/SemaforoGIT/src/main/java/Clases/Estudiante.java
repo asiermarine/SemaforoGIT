f130f51bc8b131b7eca646c0df5747b786f4d6f4
Vespertino <Vespertino@DESKTOP-5LQNC3U.localdomain>
2021-05-23 23:37:49 +0200

    JavaDoc añadido a clase ESTUDIANTE

--- Documents/NetBeansProjects/SemaforoGIT/src/main/java/Clases/Estudiante.java
+++ Documents/NetBeansProjects/SemaforoGIT/src/main/java/Clases/Estudiante.java
@@ -1,15 +1,31 @@
 package clases;
+/**
+ * Aquí define el objeto Estudiante que hereda de Persona.
+ * @author Asier Mariné 1ºDAW
+ * @version 15.0.1
+ * @see Persona
+ */
 public class Estudiante extends Persona{
-	// Incluye un metodo de clase que se une a los heredados
 	int i_Curso;
+        /**
+         * Constructor de objetos de tipo Estudiante.
+         */
     	public Estudiante() {
 		i_Edad=25;
-		s_Nombre = "Francisco";
+		s_Nombre = "Luis";
 		i_Curso = 1;
 	}
+        /**
+         * Muestra por pantalla nombre, curso y edad de ESTUDIANTE.
+         */
 	public void presentarse(){
-	System.out.println("Soy " + s_Nombre + " Alumno de " + Integer.toString(i_Curso) + " y 	tengo una edad de: " + Integer.toString(i_Edad));	
+	System.out.println("Soy " + s_Nombre + " Alumno de " + Integer.toString(i_Curso) 
+                + " y 	tengo una edad de: " + Integer.toString(i_Edad));	
     	}
+        /**
+         * Llama al método presentarse() y color() de la clase ORDENADOR.
+         * @return El color generado en la clase ORDENADOR.
+         */
 	public String preguntacolor(){
 		presentarse();
 		Ordenador mipc = new Ordenador();
